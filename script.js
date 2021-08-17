97d464b95eab061dbb643b17c6cdf6ad38ce7ff7
sareenak <43492259+sareenak@users.noreply.github.com>
2021-08-17 11:52:32 +0400

    added equal function in js for calculating values

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function display(val){
     document.getElementById('inp').value=document.getElementById('inp').value+val
 }
+function clearDisplay(){
+    document.getElementById('inp').value=""
+}
+function equal(){
+    var text=document.getElementById('inp').value
+    var result=eval(text)
+    document.getElementById('inp').value=result
+}
